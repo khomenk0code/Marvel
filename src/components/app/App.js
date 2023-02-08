4cae8aa9f35f406cefcf9ebae3529f0614459a19
Anton Khomenko <4nt.khomenko@gmail.com>
2023-02-08 16:50:06 +0200

    React routes

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,43 +1,21 @@
 import AppHeader from "../appHeader/AppHeader";
-import {useState} from "react";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-
-import decoration from '../../resources/img/vision.png';
-import ErrorBoundary from "../errorBoundary/ErrorBoundry";
-import AppBanner from "../appBanner/AppBanner";
-import ComicsList from "../comicsList/ComicsList";
-
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import {ComicsPage, MainPage} from "../pages";
 
 const App = () => {
-    const [selectedChar, setChar] = useState(null)
-
-
-   const onCharSelected = (id) => {
-       setChar(id)
-    }
 
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                {/*<ErrorBoundary>*/}
-                {/*     <RandomChar/>*/}
-                {/*</ErrorBoundary>*/}
-                {/*<div className="char__content">*/}
-                {/*    <ErrorBoundary>*/}
-                {/*        <CharList onCharSelected={onCharSelected}/>*/}
-                {/*    </ErrorBoundary>*/}
-                {/*    <ErrorBoundary>*/}
-                {/*        <CharInfo charId={selectedChar}/>*/}
-                {/*    </ErrorBoundary>*/}
-                {/*</div>*/}
-                {/*<img className="bg-decoration" src={decoration} alt="vision"/>*/}
-                <AppBanner/>
-                <ComicsList/>
-            </main>
-        </div>
+      <Router>
+          <div className="app">
+              <AppHeader/>
+              <main>
+                  <Routes>
+                      <Route path="/" element={<MainPage/>}/>
+                      <Route path="/comics" element={<ComicsPage/>}/>
+                  </Routes>
+              </main>
+          </div>
+      </Router>
     )
 }
 
